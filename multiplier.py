d0f368ba77997302da5855570c44033577df3520
masahiro <m.takahashi.public+git@gmail.com>
2018-05-16 12:04:33 +0900

    first draft

---
+++ multiplier.py
@@ -0,0 +1,24 @@
+#!/usr/local/bin/python3
+
+import numpy as np
+import chainer.functions as F
+import chainer.links as L
+from chainer import Variable, optimizers
+
+model = L.Linear(1,1)
+print(model.W.data)
+print(model.b.data)
+x = Variable(np.array([[i] for i in range(10)],dtype=np.float32))
+y = model(x)
+print(y.data)
+t = Variable(np.array([[i*2] for i in range(10)],dtype=np.float32))
+optimizer = optimizers.SGD()
+optimizer.setup(model)
+time = 50
+for i in range(0,time):
+    optimizer.zero_grads()
+    y = model(x)
+    loss = F.mean_squared_error(y,t)
+    loss.backward()
+    optimizer.update()
+
