1399a297f7c6ed94a9546c3bb413fcdd8451ba6b
masahiro <m.takahashi.public@gmail.com>
2018-05-17 20:12:26 +0900

    iris revised

--- iris.py
+++ iris.py
@@ -1,10 +1,13 @@
 #!/usr/local/bin/python3
 
+# page 52
+
 import numpy as np
 import chainer.functions as F
 import chainer.links as L
 from chainer import Variable, optimizers
-from chainer import FunctionSet
+#from chainer import FunctionSet
+from chainer import Chain
 import matplotlib.pyplot as plt
 from sklearn import datasets
 
@@ -24,7 +27,8 @@ ytrain = target[index[0:75],:]
 xtest = X[index[75:150],:]
 yans = target[index[75:150]]
 
-NNset = FunctionSet(l1 = F.Linear(4,3))
+#NNset = FunctionSet(l1 = L.Linear(4,3))
+NNset = Chain(l1 = L.Linear(4,3))
 
 def model(x):
     y = F.sigmoid(NNset.l1(x))
@@ -46,3 +50,12 @@ for i in range(Tall):
     loss.backward()
     optimizer.update()
     train_loss.append(loss.data)
+
+plt.figure(figsize=(8,6))
+plt.plot(range(Tall),train_loss)
+plt.title('optimization vol2')
+plt.xlabel('step')
+plt.ylabel('loss function')
+plt.xlim([0,Tall])
+plt.ylim([0,0.5])
+plt.show()
